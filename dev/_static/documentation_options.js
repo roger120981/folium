2e0901b4cfedf54199584a533a31137dbae951a1
roger120981 <roger120981@users.noreply.github.com>
2026-01-02 05:24:00 +0000

    deploy: 460e8bc3874c52b6a2297689f6dd534ea3c44efa

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev2048+gc90abbe26',
+    VERSION: '0.1.dev2066+g460e8bc38',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
